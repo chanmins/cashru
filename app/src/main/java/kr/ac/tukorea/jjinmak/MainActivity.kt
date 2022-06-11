61bd4e6fe73f92f28c66fa0e4f9ea094dc8278b8
chanmin <chanmin080@gmail.com>
2022-06-12 06:27:29 +0900

    cm

--- app/src/main/java/kr/ac/tukorea/jjinmak/MainActivity.kt
+++ app/src/main/java/kr/ac/tukorea/jjinmak/MainActivity.kt
@@ -5,30 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import me.relex.circleindicator.CircleIndicator3
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val indicator: CircleIndicator3 = findViewById(R.id.indicator)
         val view_pager: ViewPager2 = findViewById(R.id.viewpager)
         val tabLayout: TabLayout = findViewById(R.id.tab_layout)
         val pagerAdapter = ParentFragmentPagerAdapter(this)
-
         view_pager.adapter = pagerAdapter
         view_pager.setAdapter(pagerAdapter)
 
-        indicator.setViewPager(view_pager)
-        indicator.createIndicators(5,0);
-        indicator.animatePageSelected(2)
-
         TabLayoutMediator(tabLayout, view_pager) { tab, position ->
-            val tabNames = listOf("홍보","축제","명소")
+            val tabNames = listOf("홍보"  ,"축제"   ,"명소")
             tab.text = tabNames[position]
         }.attach()
-
     }
 }
-
-
